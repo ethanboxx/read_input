9ddf3363469cdbf260316a72cbfab128d7746873
Ethan Brierley <ethanboxx@gmail.com>
2019-01-01 23:32:46 +0000

    update thoughts

--- examples/yes_no.rs
+++ examples/yes_no.rs
@@ -7,21 +7,32 @@ use read_input::prelude::*;
 use std::str::FromStr;
 
 #[derive(Debug)]
-struct Choice(bool);
+enum Choice {
+    Positive,
+    Negative,
+}
+
+use self::Choice::*;
 
 #[derive(Debug)]
-struct ChoiceParseError {}
+struct ChoiceParseError {
+    unknown: String,
+}
 
 impl FromStr for Choice {
     type Err = ChoiceParseError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match char::from_str(s) {
-            _ => Err(ChoiceParseError {}),
+        match s.to_lowercase().as_str() {
+            "y" | "yes" | "true" | "positive" => Ok(Positive),
+            "n" | "no" | "false" | "negative" => Ok(Negative),
+            _ => Err(ChoiceParseError {
+                unknown: s.to_string(),
+            }),
         }
     }
 }
 
 fn main() {
-    println!("You inputted\n{:#?}", input_new::<Choice>().get());
+    println!("You inputted\n{:#?}", input::<Choice>().get());
     dont_disappear::enter_to_continue::default();
 }
